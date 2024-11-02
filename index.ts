761b6c0daedff2155dcabef6a04a0eb2a0839519
amr641 <amr514863@gmail.com>
2024-11-02 09:00:06 +0200

    first two algorithims

---
+++ index.ts
@@ -0,0 +1,41 @@
+// binary search O(log n)
+const binarySearch = function (item: number, arr: number[]): number | string {
+    let low = 0
+    let high = arr.length - 1
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        let guess = arr[mid]
+        if (guess == item) return mid
+        else if (guess > item) high = mid - 1
+        else if (guess < item) low = mid + 1
+
+    }
+    return "none"
+}
+// console.log(binarySearch(40,[1,2,3,4,5,6,7,8,9,10]));
+
+
+// selection sort
+const findSmallestIndex = function (arr: number[]) {
+    let smallest = arr[0];
+    let smallestIndex = 0
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i]
+            smallestIndex = i
+        }
+    }
+    return smallestIndex
+}
+let selectionSort = function (arr: number[]) {
+    let smallest: number;
+    let newArray: number[] = []
+    while(arr.length>0){
+        smallest = findSmallestIndex(arr)
+        newArray.push(arr[smallest])
+        arr.splice(smallest, 1)
+    }
+    return newArray
+}
+console.log(
+    selectionSort([3, 2, 4, 5, 0, 1]));
